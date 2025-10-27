80b4d0c1b07238f49eac28658cd9ff947ffa58f5
Salima IT <salma.it.consulting@gmail.com>
2025-10-27 21:45:56 +0100

    seed and jinja

---
+++ sali_dbt_tuto/analyses/jinja-1.sql
@@ -0,0 +1,1 @@
+{%- set var_name = "salma azi" -%} {{- var_name -}}  
---
+++ sali_dbt_tuto/analyses/jinja-2.sql
@@ -0,0 +1,4 @@
+{%-   set apples = [ "orange" , "kiwi" , "gala" , "fuji" , "honey"]   -%}
+{%  for i in apples %}
+     {{ i }}
+{% endfor %} 
